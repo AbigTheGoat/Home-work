c0ce8fe7c0880abde1ce6e3a672c5edeb9f151a0
Mark Hanherly Abig <mark.hanherly@gmail.com>
2024-11-03 17:23:43 +0400

    List Homework.py

--- List Homework.py
+++ List Homework.py
@@ -18,6 +18,13 @@ print(numbers)
 names = ['Alice', 'Bob', 'Charlie', 'David', 'Emma']
 print(names[0:3])
 
+#Create a list of numbers from 1 to 10 and use a loop to print each number squared.
+numbr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+snumbr = []
+for x in numbr:
+    snumbr.append( x **2 )
+print(snumbr)
+
 #reate an empty list called shopping_cart. Add the items 'milk', 'bread', and 'eggs' to it using the .append() method. Then use .extend() to add ['butter', 'cheese'] to the list. Print the final list.
 shopping_cart = []
 shopping_cart.append(['milk', 'bread', 'eggs'])
